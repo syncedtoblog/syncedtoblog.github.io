52ae33df87a2472bb4c724cee6d6f41ea4452e29
tss101 <tshah101@gmail.com>
2021-02-01 17:40:10 +0000

    changed code for second crime article, second chart

--- datascripts/2021-01-30-Impact-Covid-19-On-UK-Crime-2/crimebreakdown.js
+++ datascripts/2021-01-30-Impact-Covid-19-On-UK-Crime-2/crimebreakdown.js
@@ -60,6 +60,8 @@ function load_chart_2(){
                 data2019 = data2019.forEach(function (d) {return to_numeric(rename_months(d), 2019) });
                 data2020 = data2020.forEach(function (d) {return to_numeric(rename_months(d), 2020) });
                 var dataCombined = [].concat(data2018, data2019, data2020)
+                console.log("dataCombined")
+                console.log(dataCombined)
                 var dataset = {'2018':data2018 , '2019': data2019, '2020': data2020 }
                 var colors = {'2018': '#f65635' , '2019':'#4BAEE8' , '2020':'#ADE84B' }
 
