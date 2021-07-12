db3451866a2124f1f5ee15e61f973f9e4db790ca
Tejas <tejas@flexmachina.ai>
2021-07-12 15:01:38 +0100

    changed colours for SF article

--- datascripts/311-complaints-articles-scripts/main_ts_multicity.js
+++ datascripts/311-complaints-articles-scripts/main_ts_multicity.js
@@ -200,11 +200,12 @@ var colourMap = {
     'Water': "#0d5ac1",
     'Homeless': "#14a9ad",
     'Dirty': "#a4e43f",
+    'Waste': "#f2510e",
     'Covid': "#d298e2",
     'Pests': "#c0a43c"
 }
 
-var colourSet = ["#f2510e" ,"#651be6" ,"#79806e" ,"#61da5e" ,"#cd2f00" ,"#9348af" ,"#01ac53" ,"#c5a4fb" ,"#996635","#b11573" ,"#4bb473" ,"#75d89e" ,"#2f3f94" ,"#2f7b99" ,"#da967d" ,"#34891f" ,"#b0d87b" ,"#ca4751" ,"#7e50a8" ,"#c4d647" ,"#e0eeb8" ,"#11dec1" ,"#289812" ,"#566ca0" ,"#ffdbe1" ,"#2f1179" ,"#935b6d" ,"#916988" ,"#513d98","#aead3a", "#9e6d71", "#4b5bdc", "#0cd36d","#250662", "#cb5bea", "#228916", "#ac3e1b", "#df514a", "#539397", "#880977","#f697c1", "#ba96ce", "#679c9d", "#c6c42c", "#5d2c52", "#48b41b", "#e1cf3b","#5be4f0", "#57c4d8", "#a4d17a", "#225b8", "#be608b", "#96b00c", "#088baf","#f158bf", "#e145ba", "#ee91e3", "#05d371", "#5426e0", "#4834d0", "#802234","#6749e8", "#0971f0", "#8fb413", "#b2b4f0", "#c3c89d", "#c9a941", "#41d158","#fb21a3", "#51aed9", "#5bb32d", "#807fb","#f205e6", "#a48a9e"];
+var colourSet = ["#651be6" ,"#79806e" ,"#61da5e" ,"#cd2f00" ,"#9348af" ,"#01ac53" ,"#c5a4fb" ,"#996635","#b11573" ,"#4bb473" ,"#75d89e" ,"#2f3f94" ,"#2f7b99" ,"#da967d" ,"#34891f" ,"#b0d87b" ,"#ca4751" ,"#7e50a8" ,"#c4d647" ,"#e0eeb8" ,"#11dec1" ,"#289812" ,"#566ca0" ,"#ffdbe1" ,"#2f1179" ,"#935b6d" ,"#916988" ,"#513d98","#aead3a", "#9e6d71", "#4b5bdc", "#0cd36d","#250662", "#cb5bea", "#228916", "#ac3e1b", "#df514a", "#539397", "#880977","#f697c1", "#ba96ce", "#679c9d", "#c6c42c", "#5d2c52", "#48b41b", "#e1cf3b","#5be4f0", "#57c4d8", "#a4d17a", "#225b8", "#be608b", "#96b00c", "#088baf","#f158bf", "#e145ba", "#ee91e3", "#05d371", "#5426e0", "#4834d0", "#802234","#6749e8", "#0971f0", "#8fb413", "#b2b4f0", "#c3c89d", "#c9a941", "#41d158","#fb21a3", "#51aed9", "#5bb32d", "#807fb","#f205e6", "#a48a9e"];
 
 var colourOther = '#999999'
 
@@ -313,6 +314,9 @@ var prepVariables = function(displaydt, data_, data_format_fn) {
         }
         else if (includesKeyword(data[k],['homeless', 'encampment'])) {
             data[k].complaint_class = 'Homeless'
+        }
+        else if (includesKeyword(data[k],['waste'], true)) {
+            data[k].complaint_class = 'Waste'
         }
         else if (includesKeyword(data[k],['dirty', 'cleaning'])) {
             data[k].complaint_class = 'Dirty'
